61f2632ac640d90209ef17457c5a4d34d3f57dc4
Lejun Chen <chenlejun@gmail.com>
2025-03-27 20:08:32 +0100

    Create APIs: controllers, models, etc

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }    =string.Empty;
         public string? Description { get; set; }
-        //public List<PointOfInterestDto> PointsOfInterest { get; internal set; }
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }=new List<PointOfInterestDto>();
+
+        public int NumberOfPointsOfInterest => PointsOfInterest.Count;
+
     }
 }
