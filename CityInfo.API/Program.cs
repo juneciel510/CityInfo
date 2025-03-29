c2aabe9b8b2a887c3444c8222f1dd7a0b3e9034a
Lejun Chen <chenlejun@gmail.com>
2025-03-29 19:27:15 +0100

    Add Data base (#5)
    * Add entity class
    
    * Install Entity framework  core,  install sqlite
    Add DBcontext, add database service
    migrate & update database
    
    * Seed database
    
    * move connection string from program.cs to app setting file( not safe enough, just for demo)

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,6 +1,8 @@
 using CityInfo.API;
+using CityInfo.API.DbContexts;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -44,6 +46,8 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 #endif
 
 builder.Services.AddSingleton<CitiesDataStore>();
+builder.Services.AddDbContext<CityInfoContext>(
+dbContextOptions => dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
 // Configure the HTTP request pipeline.
 var app = builder.Build();
